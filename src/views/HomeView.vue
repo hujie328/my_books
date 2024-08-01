8a6277e198700e38a2c4c636adc30b5d4b833eb0
胡杰 <hujie_0328@163.com>
2024-08-01 18:01:54 +0800

    首页导航

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,45 @@
   <div class="home">
     <head>
       <nav>
-        <div class="nav_bar">
-          
+        <div class="nav_bar_block">
+          <el-row align="middle" justify="center">
+            <!-- 顶部导航 -->
+            <el-col :xl="9" :lg="9" :xs="0">
+              <div class="bars_list">
+                <div class="bar_item">
+                  <i class="iconfont icon-home"></i>
+                  <span>首页</span>
+                </div>
+                <div class="bar_item">
+                  <i class="iconfont icon-shuoshuo"></i>
+                  <span>说说</span>
+                </div>
+                <div class="bar_item">
+                  <i class="iconfont icon-time"></i>
+                  <span>归档</span>
+                </div>
+                <div class="bar_item">
+                  <i class="iconfont icon-biji"></i>
+                  <span>森林书</span>
+                </div>
+              </div>
+            </el-col>
+            <!-- 搜索 -->
+            <el-col :xl="9" :lg="9">
+              <div class="seach_box">
+                <div :class="['seach', isExpend ? 'expend' : '']">
+                  <i class="iconfont icon-search seach_icon" @click="seachClickHandle"></i>
+                  <input type="text" placeholder="搜索点什么呢" class="seach_input" />
+                </div>
+              </div>
+            </el-col>
+            <!-- 只有移动端显示 -->
+            <el-col :xs="12" :sm="0"></el-col>
+          </el-row>
         </div>
       </nav>
     </head>
+    
     <router-view />
     <snowflake></snowflake>
   </div>
@@ -14,6 +48,13 @@
 
 <script setup>
 import snowflake from '@/components/snowflake.vue';
+
+const isExpend = ref(false)
+
+const seachClickHandle = () => {
+  isExpend.value = true
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -21,6 +62,7 @@ head,
 nav {
   display: block;
 }
+
 .home {
   width: 100%;
   min-height: 100vh;
@@ -39,7 +81,82 @@ nav {
     z-index: -2;
   }
 }
-.nav_bar {
+
+.nav_bar_block {
   width: 100%;
+  padding: 10px;
+
+  .bars_list {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .bar_item {
+      padding: 0 20px;
+      display: flex;
+      align-items: center;
+      color: #FFFFFF;
+      font-size: 14px;
+
+      &:hover {
+        color: #b3beb8;
+      }
+
+      .iconfont {
+        margin-right: 6px;
+      }
+    }
+  }
+
+  .seach_box {
+    width: 100%;
+    overflow: hidden;
+    display: flex;
+    justify-content: flex-end;
+
+    .seach {
+      width: 250px;
+      display: flex;
+      border-radius: 4px;
+      padding: 12px 10px;
+      position: relative;
+      right: -220px;
+      background-color: transparent;
+      transition: all 0.3s;
+
+      &.expend {
+        background-color: #FFFFFF;
+        right: 10px;
+
+        .seach_icon {
+          color: #adb5bd;
+        }
+
+      }
+
+      .seach_icon {
+        font-size: 18px;
+        color: #FFFFFF;
+        margin-right: 10px;
+      }
+
+      .seach_input {
+        flex: 1;
+        overflow: hidden;
+        height: 100%;
+        border: none;
+        outline: none;
+        background-color: transparent;
+        color: #8898aa;
+        font-size: 14px;
+
+
+        &::placeholder {
+          color: #8898aa;
+          font-size: 14px;
+        }
+      }
+    }
+  }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,42 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// Element自动导入
 const AutoImport = require('unplugin-auto-import/webpack').default
 const Components = require('unplugin-vue-components/webpack').default
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-console.log(typeof AutoImport, Components);
-debugger
+
+
+// Element自动导入 图标
+const Icons = require('unplugin-icons/webpack').default
+const IconsResolver = require('unplugin-icons/resolver').default
+
+
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
       AutoImport({
-        resolvers: [ElementPlusResolver()],
+        imports: ['vue'],
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon',
+          })
+        ],
       }),
       Components({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [
+          ElementPlusResolver(),
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep'],
+          })
+        ]
+      }),
+      Icons({
+        autoInstall: true,
       })
     ]
   }
