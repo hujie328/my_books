fca1e6beba3f5165a7ba3494d17c5911a22367db
胡杰 <hujie_0328@163.com>
2024-08-14 11:16:24 +0800

    更新

--- src/views/home/code/index.vue
+++ src/views/home/code/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div ref="codeMain" class="code_main">
     </div>
+    <div id="previewDiv" style="background-color: #FFFFFF;">
+    </div>
+    <el-button type="primary" @click="togglePreview">测试</el-button>
 </template>
 
 <script setup>
@@ -8,15 +11,18 @@ import { ref } from 'vue';
 import { EditorView, basicSetup } from 'codemirror';
 import { EditorState } from '@codemirror/state';
 import { javascript } from '@codemirror/lang-javascript'
+import { markdown } from '@codemirror/lang-markdown'
+import { marked } from 'marked';
+
 const codeMain = ref(null);
 
 const state = EditorState.create({
-    doc: "hello",  //这是文本
-    extensions: [basicSetup, javascript()]
+    doc: "const",  //这是文本
+    extensions: [basicSetup, javascript(), markdown()]
 })
-
+let editorView = ''
 onMounted(() => {
-    const editorView = new EditorView({
+    editorView = new EditorView({
         parent: codeMain.value,
         state
     });
@@ -28,9 +34,20 @@ onMounted(() => {
             insert: ' world!'
         }
     });
-    const updatedCode = editorView.state.doc.toString();
-    console.log(updatedCode);
+    const transaction = editorView.state.update({
+        changes: { from: editorView.state.doc.length, insert: '新的文本' },
+    });
+    editorView.dispatch(transaction);
+
+    // const updatedCode = editorView.state.doc.toString();
+    // console.log(updatedCode);
 });
+const togglePreview = () => {
+    const previewDiv = document.getElementById('previewDiv');
+    const markdownContent = editorView.state.doc.toString();
+    const highlightedContent = marked(markdownContent)
+    previewDiv.innerHTML = highlightedContent;
+};
 </script>
 
 <style scoped></style>
